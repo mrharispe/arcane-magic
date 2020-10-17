aa3158c595e56a513ed6ebcd4c60a8484b1f991f
Martin Rodriguez <mrharispe@gmail.com>
2020-10-17 10:57:26 -0400

    Add style to Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 export interface IButton {
     /** The text to be displayed inside the button. */
     text: string,
 }
 
+const Wrapper = styled.button`
+  letter-spacing: 1px;
+  background: #1d5ae0;
+  color: whitesmoke;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
 const Button = ({ text }: IButton) => (
-    <button>{text}</button>
+    <Wrapper>{text}</Wrapper>
 )
 
 export default Button
